f6082080439f3e260d3b43e71b41bc7b4470a7e8
tomat <xxlennygamerxx@gmail.com>
2024-04-15 11:21:28 -0700

    Working mod.

--- src/tomat-enemies_fade_on_low_health/mod_main.gd
+++ src/tomat-enemies_fade_on_low_health/mod_main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const AUTHORNAME_MODNAM_DIR := "tomat-enemies_fade_on_low_health"
+const AUTHORNAME_MODNAME_DIR := "tomat-enemies_fade_on_low_health"
 const AUTHORNAME_MODNAME_LOG_NAME := "tomat-enemies_fade_on_low_health:Main"
 
 var mod_dir_path := ""
@@ -8,11 +8,11 @@ var mod_dir_path := ""
 var Global:Node
 
 func _init() -> void:
-	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(AUTHORNAME_MODNAM_DIR)
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(AUTHORNAME_MODNAME_DIR)
 	install_script_extensions()
 
 func install_script_extensions() -> void:
-	ModLoaderMod.install_script_extension("res://mods-unpacked/tomat-enemies_fade_on_load_health/extensions/src/enemy/enemy.gd")
+	ModLoaderMod.install_script_extension("res://mods-unpacked/tomat-enemies_fade_on_low_health/extensions/src/enemy/enemy.gd")
 
 func _ready() -> void:
 	ModLoaderLog.info("Ready!", AUTHORNAME_MODNAME_LOG_NAME)
